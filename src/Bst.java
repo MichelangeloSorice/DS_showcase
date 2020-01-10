5da524ead941a2799cb3579eaf36cecad8e93585
RED_SOCKET <msorice11@gmail.com>
2020-01-10 12:33:25 +0100

    Completed BST

--- src/Bst.java
+++ src/Bst.java
@@ -1,8 +1,19 @@
+import java.util.*;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Basic implementation of a Binary Search Tree
  * @param <T>
  */
 public class Bst<T extends Comparable<T>> {
+
+    public enum TreeTraversalOrder{
+        PREorder,
+        POSTorder,
+        INorder,
+        LEVELorder;
+    }
 
    private class Node {
        T data;
@@ -113,5 +124,129 @@ public class Bst<T extends Comparable<T>> {
         return Math.max(height(node.left), height(node.right))+1;
     }
 
+    public Iterator<T> traverse(TreeTraversalOrder order){
+        switch (order){
+            case INorder: return traverseInOrder();
+            case PREorder: return traversePreOrder();
+            case POSTorder: return traversePostOrder();
+            case LEVELorder: return traverseLevelOrder();
+            default: return null;
+        }
+    }
+
+    private Iterator<T> traversePreOrder() {
+        int expNodeCount = nodeCount; // to spot concurrent modifications
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+                return root != null && !stack.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+
+                Node node = stack.pop();
+                if(node.right != null) stack.push(node.right);
+                if(node.left != null) stack.push(node.left);
+
+                return node.data;
+            }
+        };
+    }
+
+    private Iterator<T> traversePostOrder() {
+        int expNodeCount = nodeCount; // to spot concurrent modifications
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+        stack1.push(root);
+
+        while(!stack1.isEmpty()){
+            Node node = stack1.pop();
+
+            if(node != null){
+                stack2.push(node);
+                if(node.left != null) stack1.push(node.left);
+                if(node.right != null) stack1.push(node.right);
+            }
+        }
+
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+                return root != null && !stack2.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+
+                return stack2.pop().data;
+            }
+        };
+    }
+
+    private Iterator<T> traverseInOrder() {
+        int expNodeCount = nodeCount; // to spot concurrent modifications
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        return new Iterator<T>() {
+            Node trav = root;
+            @Override
+            public boolean hasNext() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+                return root != null && !stack.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+
+                // Going as far left as possible, pushing everything into the stack
+                while(trav != null && trav.left != null){
+                    stack.push(trav.left);
+                    trav = trav.left;
+                }
+
+                Node node = stack.pop();
+
+                if(node.right != null){
+                    stack.push(node.right);
+                    trav = node.right;
+                }
+
+                return node.data;
+            }
+        };
+    }
+
+    private Iterator<T> traverseLevelOrder(){
+        int expNodeCount = nodeCount; // to spot concurrent modifications
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if(expNodeCount != nodeCount) throw new ConcurrentModificationException();
+                return root != null && !queue.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                Node node = queue.poll();
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+                return node.data;
+            }
+        };
+
+    }
 
 }
